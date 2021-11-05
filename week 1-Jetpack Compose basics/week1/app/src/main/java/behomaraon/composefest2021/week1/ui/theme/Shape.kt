b4e7c424dcf9a1f77d838a62e0ef8c0142a86073
Behomaraon <behomaraon@gmail.com>
2021-11-06 02:56:55 +0900

    Codelab Week1

---
+++ week 1-Jetpack Compose basics/week1/app/src/main/java/behomaraon/composefest2021/week1/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package behomaraon.composefest2021.week1.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
